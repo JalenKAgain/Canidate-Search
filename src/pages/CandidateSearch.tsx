3807ef6633c5d9b56ce265dab9d70a5f4fcbe9cc
JalenKAgain <jacjalen16@gmail.com>
2025-03-04 16:28:52 -0500

    candidate interface,candidate card updated

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,69 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { type FormEvent, useState } from 'react';
+
+import { searchGithub } from '../api/API';
+import CandidateCard from '../components/CandidateCard';
+
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  
+  const [] = useState({});
+
+  const [searchInput, setSearchInput] = useState<string>('');
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function searchcurrentCandidate(_event: FormEvent<HTMLFormElement>, _searchInput: string): void {
+    throw new Error('Function not implemented.');
+  }
+
+  return (
+    <>
+      <section id='searchSection'>
+        <form
+          // TODO: Add correct type to event
+          onSubmit={(event) => searchcurrentCandidate(event, searchInput)}
+        >
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='Enter Name'
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button type='submit' id='searchBtn'>
+            Search
+          </button>
+        </form>
+      </section>
+      <CandidateCard
+        currentUser={currentUser} makeSelection={function (_isSelected: boolean): void {
+          throw new Error('Function not implemented.');
+        } }
+      />
+    </>
+  );
 };
 
 export default CandidateSearch;
--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,50 @@
+import { type FormEvent, useState } from 'react';
+
+import { searchGithub } from '../api/API';
+import CandidateCard from '../components/CandidateCard';
+
 const SavedCandidates = () => {
+ 
+  const [currentCandidate, setSavedCandidates] = useState({});
+
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  
+  const addCandidate = () => {
+   
+    let parsedSavedCandidates = [];
+    const storedSavedCandidates = localStorage.getItem('SavedCandidates');
+    if (typeof SavedCandidates === 'string') {
+      parsedSavedCandidates = JSON.parse(storedSavedCandidates);
+    }
+    parsedSavedCandidates.push(SavedCandidates);
+    localStorage.setItem('SavedCandidates', JSON.stringify(parsedSavedCandidates));
+  };
+
+ 
+  
+ 
+
   return (
     <>
-      <h1>Potential Candidates</h1>
+      <section id='searchName'>
+        <form
+          // TODO: Add correct type to event
+          onSubmit={(event) => setSavedCandidates(event, searchInput)}
+        >
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='Enter Name'
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button type='submit' id='searchBtn'>
+            Search
+          </button>
+        </form>
+      </section>
+     
     </>
   );
 };
